2b85aa6233a3cb81b8184c53e21ca46be91fca25
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2024-11-30 13:03:49 -0500

    Initial Commit

---
+++ TradingCards2.0/TradingCards2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCards2_0App.swift
+//  TradingCards2.0
+//
+//  Created by Harshan Dhindsa on 2024-11-30.
+//
+
+import SwiftUI
+
+@main
+struct TradingCards2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
